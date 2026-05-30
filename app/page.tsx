e3a48fa0a538adaa16904923928bb0c03d989aa7
santy1222 <lapeseto2@outlook.com>
2026-05-30 19:48:47 +1200

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { useState, useEffect } from 'react'
 import Navbar from '@/components/Navbar'
 import TripCard from '@/components/TripCard'
